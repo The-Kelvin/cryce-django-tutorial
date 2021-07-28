85151807d7ceb1a69b96a42fa26c120bb23ffd96
The-Kelvin <kelvinmuriithi7@gmail.com>
2021-07-28 17:10:01 +0300

    Deploy app

--- todosite/settings.py
+++ todosite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import sys
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,7 +154,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-# Email_from setting
+# Email_from setting /
 
 # EMAIL_FROM_USER = os.environ.get('EMAIL_FROM_USER')
 # EMAIL_HOST = 'smtp.gmail.com'
@@ -168,3 +169,7 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_FROM_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
+
+
+
+django_heroku.settings(locals())
